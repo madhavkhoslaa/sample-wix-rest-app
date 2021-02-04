89c3cc0a3f31bb0fc09d4f24403969534d8b26c5
Madhav Khosla <28945483+madhavkhoslaa@users.noreply.github.com>
2021-02-05 00:44:18 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 // TODO: Update APP_ID with your application ID (can be found in in Wix Developers under Workspace/OAuth)
-const APP_ID = '33d60070-a0fe-4763-9cbb-09afa9a65aa9';
+const APP_ID = '36b36619-2658-45c6-8fab-12f9ab113111';
 
 // TODO: Update PUBLIC_KEY with your public Key (can be found under Workspace/Webhooks/Public-Key)
-const PUBLIC_KEY= '-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAik619s5/7vr8gcAJcE60\nBj868t9Kn+Pa9JpJm2jWJw320Z//dCO8B4FFKEhpXr5ad4xOxPtO9n1OkypBt2TD\nL6YQA28HMUHOcn5t8go/zW7Xc4++kQVX3jxwbrCN2YXydkAh0gEAq7P3OiEVq7aT\nGCCS/UTZ+89JiTlnwPaxP9QsBCSP2QR6cKXRMJ9ZQnFThhr0k+Z50p1uncg+5ELu\njX5g/sgZ481eind6fJItwm/qgNZoX7rizlsM4SGt7aqMRWaeIQEXajgQCjf79MPs\nuQTTarzpZo0uO4i4A7iELJz+/yOeRpwQNROfc3s1XceKoOeWgeCNhPQXg0N50T77\nXQIDAQAB\n-----END PUBLIC KEY-----'
+const PUBLIC_KEY= ''
 
 exports.APP_ID = APP_ID;
 exports.PUBLIC_KEY = PUBLIC_KEY;
