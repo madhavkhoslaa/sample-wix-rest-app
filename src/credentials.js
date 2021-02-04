a9dc3ec0219dcedaeddad9c81368ad51b82ff340
Madhav Khosla <28945483+madhavkhoslaa@users.noreply.github.com>
2021-02-05 00:42:52 +0530

    Update credentials.js

--- src/credentials.js
+++ src/credentials.js
@@ -1,4 +1,4 @@
 // TODO: Update APP_SECRET with your application secret key (can be found in Wix Developers under Workspace/OAuth)
-const APP_SECRET = 'f8e15ecb-381e-4701-a72a-fedff986d0b4';
+const APP_SECRET = 'a452b01a-6835-41e2-a0e1-f479e581622d';
 
 exports.APP_SECRET = APP_SECRET;
